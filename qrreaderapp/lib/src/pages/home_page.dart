f9eaf4eda92309bafd64bfe726414861bbe15c22
Anegelisai <63125934+Anegelisai@users.noreply.github.com>
2020-08-13 00:12:25 -0500

    firsth commit

---
+++ qrreaderapp/lib/src/pages/home_page.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:qrreaderapp/src/pages/direcciones_page.dart';
+import 'package:qrreaderapp/src/pages/mapas_page.dart';
+
+
+class HomePage extends StatefulWidget {
+  
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  int currentIndex=0;
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("QR Scanner"),
+        actions: [
+          IconButton(
+            icon: Icon (Icons.delete_forever),
+            onPressed: (){},
+            )
+        ],
+      ),
+
+      body: _callPage(currentIndex),
+
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Theme.of(context).primaryColor,
+        child: Icon(Icons.filter_center_focus),
+        onPressed: _scanQR,        
+      ),
+      
+      
+      bottomNavigationBar: _crearBottonNavigation(),
+    );
+  }
+
+  _scanQR(){
+
+    
+
+  }
+
+  Widget _crearBottonNavigation(){
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: (index){
+          setState(() {
+          currentIndex =index;            
+          });
+      },
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.map),
+          title: Text("mapa")
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.swap_vert),
+          title: Text("direcciones")
+        ),
+      ],
+    );
+
+  }
+
+  Widget _callPage(int paginaActual) {
+
+    switch (paginaActual) {
+      case 0:
+        return MapasPage();
+        break;
+      case 1:
+        return DireccionesPage();
+        break;
+      default:
+        return MapasPage();
+        break;
+      
+    }
+
+  }
+}
