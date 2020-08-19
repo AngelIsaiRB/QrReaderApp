f6a620e2a913a44357edf5b32f4df8d26ea6e869
Anegelisai <63125934+Anegelisai@users.noreply.github.com>
2020-08-19 14:39:12 -0500

    mothods block

---
+++ qrlector/lib/src/bloc/scans_block.dart
@@ -0,0 +1,59 @@
+
+
+
+
+import 'dart:async';
+
+import 'package:qrlector/src/models/scan_model.dart';
+import 'package:qrlector/src/providers/db_provider.dart';
+
+class ScansBloc{
+
+  static final ScansBloc _singleton =new ScansBloc._internal();
+
+  factory ScansBloc(){
+    return _singleton;
+  }
+
+  ScansBloc._internal(){
+  obtenerScans();
+  }
+
+  final _scansController = StreamController<List<ScanModel>>.broadcast();
+
+  Stream<List<ScanModel>> get scansStream => _scansController.stream;
+
+  dispose(){
+    _scansController?.close();
+  }
+
+  obtenerScans() async {
+    _scansController.sink.add( await DBProvider.db.getTodosScans());    
+  }
+
+  agregarsacan(ScanModel scan) async {
+    await DBProvider.db.nuevoScan(scan);
+    obtenerScans();
+  }
+
+  borrarScan(int id) async{
+
+    await DBProvider.db.deleteScan(id);
+    obtenerScans();
+  }
+
+  borrarTodosScans() async{
+    DBProvider.db.deleteAll();
+    obtenerScans();
+
+  }
+
+
+  
+
+
+
+
+}
+
+
